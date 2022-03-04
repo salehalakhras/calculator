4c56ef34290380432dacd9e76760ccc267db2fd6
saleh <salehalakhras2000@gmail.com>
2022-03-04 16:06:30 +0200

    added all buttons

---
+++ script.js
@@ -0,0 +1,20 @@
+const add = (num1, num2) => num1 + num2;
+const subtract = (num1, num2) => num1 - num2;
+const multiply = (num1, num2) => num1 * num2;
+const divide = (num1, num2) => num1 / num2;
+
+const operate = (num1, operator, num2) => {
+    switch (operator) {
+        case '+':
+            return add(num1, num2);
+        case '-':
+            return subtract(num1, num2);
+        case '*':
+            return multiply(num1, num2);
+        case '/':
+            return divide(num1, num2);
+        default:
+            return 'ERROR!';
+
+    }
+}
