a118ba0a953bb65aaef24688873ee018da0ae01d
saleh <salehalakhras2000@gmail.com>
2022-03-04 17:13:33 +0200

    added a function to clear the display and a function to check if an operation is complete

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const add = (num1, num2) => num1 + num2;
 const subtract = (num1, num2) => num1 - num2;
 const multiply = (num1, num2) => num1 * num2;
 const divide = (num1, num2) => num1 / num2;
+const display = document.querySelector('.display');
 var displayValue;
 const operate = (num1, operator, num2) => {
     switch (operator) {
@@ -20,7 +21,6 @@ const operate = (num1, operator, num2) => {
 }
 
 const drawToDisplay = (operand) => {
-    const display = document.querySelector('.display');
     display.textContent += operand;
     displayValue = display.textContent;
 }
@@ -32,9 +32,28 @@ digitBtn.forEach(btn => {
     });
 })
 
+const isFullOperation = (operation) => {
+    if (operation.split(' ').length === 3) {
+        return true;
+    } else
+        return false;
+}
+
+const clearDisplay = () => {
+    display.textContent = '';
+}
 const operators = document.querySelectorAll('.operator');
 operators.forEach(operator => {
     operator.addEventListener('click', e => {
-
+        var previousOperation = displayValue;
+        drawToDisplay(` ${e.target.innerText} `);
+        if (isFullOperation(previousOperation)) {
+            previousOperation = previousOperation.split(' ');
+            var result = operate(previousOperation[0], previousOperation[1], previousOperation[2]);
+            console.log(typeof(previousOperation));
+            result += ` ${displayValue[displayValue.length -2]} `;
+            clearDisplay();
+            drawToDisplay(result);
+        }
     })
 })
